1e2aece936c859dbbffb79a04444b4e53a15a6a6
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-10-22 18:53:38 -0400

    Commit Mix app skeleton

---
+++ lib/ecg_service.ex
@@ -0,0 +1,18 @@
+defmodule EcgService do
+  @moduledoc """
+  Documentation for EcgService.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EcgService.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
