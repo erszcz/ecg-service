f228f48bd7348e599e88a4f95647f0940bd2d55a
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-10-22 18:56:43 -0400

    Add deps

--- mix.exs
+++ mix.exs
@@ -10,24 +10,13 @@ defmodule EcgService.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~> 1.0.0"},
+     {:plug, "~> 1.0"},
+     {:ecg, github: "erszcz/ecg", ref: "a03db02"}]
   end
 end
